b13e27f938f07790d9141c4262eb77e252e895cd
MUNSI Patrick <patrick.munsi@ethias.be>
2020-12-31 00:22:12 +0100

    Mise en plasce des Vues & URLs pour l'API

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'movies.apps.MoviesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
